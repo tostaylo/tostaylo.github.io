0adf4422a2a02db364316b771d8b69f0d593f9df
Torre <tostaylo1@gmail.com>
2020-08-08 16:42:57 -0500

    removing underscores from package files

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@
 // will "boot" the module and make it ready to use. Currently browsers
 // don't support natively imported WebAssembly as an ES module, but
 // eventually the manual initialization won't be required!
-import init from './pkg/rust_fel_portfolio.js';
+import init from './pkg/rust-fel-portfolio.js';
 
 async function run() {
 	// First up we need to actually load the wasm file, so we use the
--- src/site_info.rs
+++ src/site_info.rs
@@ -4,9 +4,30 @@ pub fn site_info() -> rust_fel::Element {
     let site_info_text = rust_fel::html(format!(
         "<div> 
           <h2>Site Info</h2>
+         <p> 
+             You are viewing a site built with Rust and Web Assembly.
+             Here are the reasons I chose Rust and Web Assembly.
+             Additonally, I have invented my own front-end-framework ( Yes the world need another one ).
+             It's not very good. But it got the job done.
+        </p>
+        <p>
+            Here is a simple rust_fel functional component!
+        </p>
           <pre>
-            <code>pub fn what(){{
-                        }}
+            <code>
+                use rust_fel;
+
+                pub fn site_info() -> rust_fel::Element {{
+                let site_info = rust_fel::Element::new(
+                        \"div\".to_owned(),
+                        rust_fel::Props {{}}
+                            class_name: Some(format!(\"site-info\")),
+                            children: Some(vec![site_info_text]),
+                            ..Default::default()
+                        }},
+                    );
+                    site_info
+                }}
             </code>
          </pre>
         </div>"
