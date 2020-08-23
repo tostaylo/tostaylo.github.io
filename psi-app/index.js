2868f4fde8a4a910cdeea2122889a60ce95db528
Torre <tostaylo1@gmail.com>
2020-08-23 15:33:28 -0500

    rename psi-app.js

--- psi-app/index.js
+++ psi-app/index.js
@@ -7,7 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import init from './pkg/psi_app.js';
+import init from './pkg/psi-app.js';
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         yield init();
--- psi-app/index.ts
+++ psi-app/index.ts
@@ -5,7 +5,7 @@
 // will "boot" the module and make it ready to use. Currently browsers
 // don't support natively imported WebAssembly as an ES module, but
 // eventually the manual initialization won't be required!
-import init from './pkg/psi_app.js';
+import init from './pkg/psi-app.js';
 
 async function run() {
 	// First up we need to actually load the wasm file, so we use the
